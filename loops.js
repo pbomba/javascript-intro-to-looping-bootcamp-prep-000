64f398658c2304393bf4955234ff62b5a40e1950
Paul Bomba <paul@paulbomba.com>
2018-05-12 15:15:55 +0000

    automatically backed up by learn

---
+++ loops.js
@@ -0,0 +1,14 @@
+
+
+function forLoop(array){
+  for (var i = 0; i < 25; i++) {
+  array.push(`I am ${i} strange loop${i === 0 ? '' : 's'}.`);
+  }
+  return array;
+}
+
+function whileLoop(n) {
+  while (n >= 0);
+  console.log (n);
+  n--;
+}
